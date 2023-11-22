ad02e5dabe68b49b177a896a76050e5417f331eb
Lois Andrei C. Ramos <142395913+LoisRamos@users.noreply.github.com>
2023-11-22 17:29:44 +0800

    Add files via upload

--- db_sm3101.sql
+++ db_sm3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 21, 2023 at 10:47 AM
+-- Generation Time: Nov 22, 2023 at 09:28 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -24,34 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `editparking`
+-- Table structure for table `carparking`
 --
 
-DROP TABLE IF EXISTS `editparking`;
-CREATE TABLE IF NOT EXISTS `editparking` (
+DROP TABLE IF EXISTS `carparking`;
+CREATE TABLE IF NOT EXISTS `carparking` (
   `Id_number` int NOT NULL,
-  `Name` varchar(50) DEFAULT NULL,
   `Address` varchar(50) DEFAULT NULL,
   `Position` varchar(50) DEFAULT NULL,
   `Contact_number` varchar(50) DEFAULT NULL,
   `Car_model` varchar(50) DEFAULT NULL,
   `Plate_number` varchar(50) DEFAULT NULL,
-  `Primary_color` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Id_number`)
+  `Primary_color` varchar(50) DEFAULT NULL,
+  `empid` int DEFAULT NULL,
+  `studid` int NOT NULL,
+  PRIMARY KEY (`Id_number`),
+  KEY `empid` (`empid`),
+  KEY `studid` (`studid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Dumping data for table `editparking`
+-- Dumping data for table `carparking`
 --
 
-INSERT INTO `editparking` (`Id_number`, `Name`, `Address`, `Position`, `Contact_number`, `Car_model`, `Plate_number`, `Primary_color`) VALUES
-(2020202, 'May name na', 'May Address na', 'May position na', '123450987', 'SUZUKI', 'SECRET123', 'YELLOW'),
-(78237, 'Son Goku', 'Banay-Banay Lipa', 'Teacher', '2147483647', 'Ford Mustang', 'QWE145', 'Blue'),
-(99999, 'Itachi Uchiha', 'Konoha', 'Instructor', '2147483647', 'Toyota GR Supra', 'OMG888', 'Gray'),
-(92385, 'Andrew Tate', 'Secret Address', 'Visitor', '2147483647', 'Bugatti Chiron', 'TOPG543', 'Orange'),
-(66666, 'Andrei', 'Lipa City', 'Student', '2147483647', 'Ferrari F12', 'SHH111', 'Dark-Red'),
-(789, 'Jessica', 'Hindi na Homeless', 'Wala', '976', 'Walang car', 'JKH123', 'Walang car'),
-(486123, 'Eloyyy', 'Inosloban', 'President', '2147483647', 'HYUNDAI', 'AYU890', 'Blue');
+INSERT INTO `carparking` (`Id_number`, `Address`, `Position`, `Contact_number`, `Car_model`, `Plate_number`, `Primary_color`, `empid`, `studid`) VALUES
+(1, 'Lipa City', 'Teacher', '09123456789', 'SUZUKI', 'SECRET123', 'YELLOW', 3, 5),
+(2, 'Banay-banay', 'Visitor', '09123456789', 'Ford Mustang', 'QWE145', 'BLUE', 1, 4),
+(3, 'Sto.Tomas', 'Supervisor', '09123456789', 'Toyota GR Supra', 'OMG888', 'GRAY', 2, 2),
+(4, 'Inosloban Lipa', 'Visitor', '09123456789', 'Bugatti Chiron', 'TOPG543', 'Orange', 4, 3),
+(5, 'Marawoy', 'Teacher', '09123456789', 'Ferrari F12', 'SHH111', 'Dark-red', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -66,14 +67,19 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbempinfo`
 --
 
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
-(1, 'aguila', 'nina', 'cics');
+(1, 'aguila', 'nina', 'cics'),
+(2, 'Ramos', 'Lois Andrei', 'CICS'),
+(3, 'Penera', 'Jay Troyd', 'CICS'),
+(4, 'Razon', 'Joana Marie', 'CICS'),
+(5, 'Rosa', 'Christine Ann', 'CICS'),
+(6, 'Zara', 'Von Raniel', 'CICS');
 
 -- --------------------------------------------------------
 
@@ -88,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbstudinfo`
@@ -96,7 +102,11 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
 
 INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
 (1, 'parker', 'peter', 'bsit'),
-(2, 'kent', 'clark', 'bscs');
+(2, 'kent', 'clark', 'bscs'),
+(3, 'Wood', 'Jack', 'BSCS'),
+(4, 'Gonzales', 'Lucio', 'BSIT'),
+(5, 'Miller', 'Steven', 'BSIT'),
+(6, 'Garner', 'Eula', 'BSBA');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
