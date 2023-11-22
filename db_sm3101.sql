9702fa6a12a171ec57a4f7ace084b39955693891
Kent Roxas <142395080+KentRoxas@users.noreply.github.com>
2023-11-22 14:14:34 +0800

    Add files via upload

--- db_sm3101.sql
+++ db_sm3101.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Oct 24, 2023 at 09:28 AM
--- Server version: 8.0.18
--- PHP Version: 7.3.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 22, 2023 at 05:06 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,17 +24,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbadmin`
+--
+
+DROP TABLE IF EXISTS `tbadmin`;
+CREATE TABLE IF NOT EXISTS `tbadmin` (
+  `deptUserID` int NOT NULL AUTO_INCREMENT,
+  `deptUserN` char(50) DEFAULT NULL,
+  `deptUserPass` char(50) DEFAULT NULL,
+  `deptID` int DEFAULT NULL,
+  `adminID` int DEFAULT NULL,
+  PRIMARY KEY (`deptUserID`),
+  KEY `tbadmin_ibfk_1` (`adminID`),
+  KEY `tbadmin_ibfk_2` (`deptID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbadmin`
+--
+
+INSERT INTO `tbadmin` (`deptUserID`, `deptUserN`, `deptUserPass`, `deptID`, `adminID`) VALUES
+(1, 'Cics1', 'CICS123', 1, 1),
+(2, 'Cabe1', 'CABE123', 2, 1),
+(3, 'Cas1', 'CAS123', 4, 1),
+(4, 'Cit1', 'CIT123', 3, 1),
+(5, 'Cics2', 'Cics123', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbdepartment`
+--
+
+DROP TABLE IF EXISTS `tbdepartment`;
+CREATE TABLE IF NOT EXISTS `tbdepartment` (
+  `deptID` int NOT NULL,
+  `deptName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`deptID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbdepartment`
+--
+
+INSERT INTO `tbdepartment` (`deptID`, `deptName`) VALUES
+(1, 'CICS'),
+(2, 'CABE'),
+(3, 'CIT'),
+(4, 'CAS');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbempinfo`
 --
 
 DROP TABLE IF EXISTS `tbempinfo`;
 CREATE TABLE IF NOT EXISTS `tbempinfo` (
-  `empid` int(11) NOT NULL AUTO_INCREMENT,
+  `empid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbempinfo`
@@ -47,17 +98,118 @@ INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbeventdetail`
+--
+
+DROP TABLE IF EXISTS `tbeventdetail`;
+CREATE TABLE IF NOT EXISTS `tbeventdetail` (
+  `eventId` int NOT NULL AUTO_INCREMENT,
+  `eventWhere` char(50) DEFAULT NULL,
+  `eventWhen` char(50) DEFAULT NULL,
+  `eventWhat` char(50) DEFAULT NULL,
+  `eventWho` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`eventId`),
+  KEY `deptId_fk` (`eventWho`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbeventdetail`
+--
+
+INSERT INTO `tbeventdetail` (`eventId`, `eventWhere`, `eventWhen`, `eventWhat`, `eventWho`) VALUES
+(1, 'BSU LIPA CAMPUS', 'December 1,2023', 'CICS Week', 'All Students');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbeventview`
+--
+
+DROP TABLE IF EXISTS `tbeventview`;
+CREATE TABLE IF NOT EXISTS `tbeventview` (
+  `srCode` varchar(50) DEFAULT NULL,
+  `section` varchar(25) DEFAULT NULL,
+  `deptName` varchar(50) DEFAULT NULL,
+  `eventWhat` varchar(50) DEFAULT NULL,
+  `evdate` date DEFAULT NULL,
+  `evtime` time DEFAULT NULL,
+  KEY `section` (`section`),
+  KEY `deptName` (`deptName`),
+  KEY `eventWhat` (`eventWhat`),
+  KEY `srCode` (`srCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbeventview`
+--
+
+INSERT INTO `tbeventview` (`srCode`, `section`, `deptName`, `eventWhat`, `evdate`, `evtime`) VALUES
+('21-34890', 'SM-3101', 'CICS', 'CICS Week', '2023-11-22', '04:58:02'),
+('21-37506', 'SM-3101', 'CICS', 'CICS Week', '2023-11-22', '05:04:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsecurity`
+--
+
+DROP TABLE IF EXISTS `tbsecurity`;
+CREATE TABLE IF NOT EXISTS `tbsecurity` (
+  `adminId` int NOT NULL,
+  `adminUserN` char(50) DEFAULT NULL,
+  `adminPass` char(50) DEFAULT NULL,
+  PRIMARY KEY (`adminId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbsecurity`
+--
+
+INSERT INTO `tbsecurity` (`adminId`, `adminUserN`, `adminPass`) VALUES
+(1, 'Admin1', 'Admin111');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbstudent`
+--
+
+DROP TABLE IF EXISTS `tbstudent`;
+CREATE TABLE IF NOT EXISTS `tbstudent` (
+  `srCode` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `studName` char(50) DEFAULT NULL,
+  `course` char(25) DEFAULT NULL,
+  `section` varchar(25) DEFAULT NULL,
+  `studPass` varchar(50) NOT NULL,
+  `deptName` varchar(30) NOT NULL,
+  PRIMARY KEY (`srCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbstudent`
+--
+
+INSERT INTO `tbstudent` (`srCode`, `studName`, `course`, `section`, `studPass`, `deptName`) VALUES
+('21-32370', 'John Paul Sabillo', 'BSIT', 'SM-3101', '123', 'CICS'),
+('21-34890', 'Kent Aldrin Roxas', 'BSIT', 'SM-3101', '123', 'CICS'),
+('21-34916', 'Mark Joshua Fain', 'BSIT', 'SM-3101', '123', 'CICS'),
+('21-36529', 'John Lloyd Lasay', 'BSIT', 'SM-3101', '123', 'CICS'),
+('21-37506', 'Juliana Beatriz Custodio', 'BSIT', 'SM-3101', '123', 'CICS');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbstudinfo`
 --
 
 DROP TABLE IF EXISTS `tbstudinfo`;
 CREATE TABLE IF NOT EXISTS `tbstudinfo` (
-  `studid` int(11) NOT NULL AUTO_INCREMENT,
+  `studid` int NOT NULL AUTO_INCREMENT,
   `lastname` varchar(25) NOT NULL,
   `firstname` varchar(25) NOT NULL,
   `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbstudinfo`
@@ -66,6 +218,23 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
 INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
 (1, 'parker', 'peter', 'bsit'),
 (2, 'kent', 'clark', 'bscs');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  ADD CONSTRAINT `tbadmin_ibfk_1` FOREIGN KEY (`adminID`) REFERENCES `tbsecurity` (`adminId`),
+  ADD CONSTRAINT `tbadmin_ibfk_2` FOREIGN KEY (`deptID`) REFERENCES `tbdepartment` (`deptID`);
+
+--
+-- Constraints for table `tbeventview`
+--
+ALTER TABLE `tbeventview`
+  ADD CONSTRAINT `tbeventview_ibfk_1` FOREIGN KEY (`srCode`) REFERENCES `tbstudent` (`srCode`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
