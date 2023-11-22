b95614caa4f1f87d8e445eb126703bc06bf32320
Erika R. Magnaye <142393931+erikamagnaye@users.noreply.github.com>
2023-11-22 18:46:21 +0800

    Add files via upload

--- db_sm3101.sql
+++ db_sm3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 22, 2023 at 10:01 AM
+-- Generation Time: Nov 22, 2023 at 10:34 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `admin_tbl` (
 --
 
 INSERT INTO `admin_tbl` (`admin_id`, `empid`) VALUES
-(501, 101);
+(501, 3);
 
 -- --------------------------------------------------------
 
@@ -120,7 +120,7 @@ CREATE TABLE IF NOT EXISTS `tablecomplaints` (
   PRIMARY KEY (`complaintNumber`),
   KEY `sr-code` (`sr-code`),
   KEY `category_id` (`category_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tablecomplaints`
@@ -131,7 +131,57 @@ INSERT INTO `tablecomplaints` (`complaintNumber`, `sr-code`, `category_id`, `com
 (2, 1001, 1, 'Cheating', 'Someone is cheating during exam.', '', '2023-11-21 16:30:10', '', '0000-00-00 00:00:00'),
 (12, 1003, 5, 'Dishonesty', 'saw someone using phone during exam', '', '2023-11-21 16:30:24', '', '0000-00-00 00:00:00'),
 (7, 1004, 4, 'Humiliation', 'The teacher makes an embarrassing comment about me', '', '2023-11-21 16:30:37', '', '0000-00-00 00:00:00'),
-(13, 1, 3, 'Complaint For Student', 'hello, testing po, hehe', '', '2023-11-21 17:50:15', NULL, '0000-00-00 00:00:00');
+(13, 1, 3, 'Complaint For Student', 'hello, testing po, hehe', '', '2023-11-21 17:50:15', NULL, '0000-00-00 00:00:00'),
+(14, 1, 2, 'Complaint for Services', 'hshhshs', '', '2023-11-22 10:31:18', NULL, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbdepacc`
+--
+
+DROP TABLE IF EXISTS `tbdepacc`;
+CREATE TABLE IF NOT EXISTS `tbdepacc` (
+  `deptUserID` int NOT NULL AUTO_INCREMENT,
+  `deptUserN` char(50) DEFAULT NULL,
+  `deptUserPass` char(50) DEFAULT NULL,
+  `deptName` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`deptUserID`),
+  KEY `tbadmin_ibfk_2` (`deptName`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbdepacc`
+--
+
+INSERT INTO `tbdepacc` (`deptUserID`, `deptUserN`, `deptUserPass`, `deptName`) VALUES
+(1, 'Cics1', 'CICS123', 'CICS'),
+(2, 'Cabe1', 'CABE123', '2'),
+(3, 'Cas1', 'CAS123', '4'),
+(4, 'Cit1', 'CIT123', '3'),
+(5, 'Cics2', 'Cics123', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbdepartment`
+--
+
+DROP TABLE IF EXISTS `tbdepartment`;
+CREATE TABLE IF NOT EXISTS `tbdepartment` (
+  `deptName` varchar(25) NOT NULL,
+  PRIMARY KEY (`deptName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbdepartment`
+--
+
+INSERT INTO `tbdepartment` (`deptName`) VALUES
+('CABE'),
+('CAS'),
+('CICS'),
+('CIT');
 
 -- --------------------------------------------------------
 
@@ -156,7 +206,7 @@ CREATE TABLE IF NOT EXISTS `tbempcontact` (
 --
 
 INSERT INTO `tbempcontact` (`id`, `empid`, `password`, `email`, `contact_no`, `address`) VALUES
-(901, 101, 'staff', 'balazon@gmail.com', 9123456789, 'Lipa City, Batangas');
+(901, 3, 'e00cf25ad42683b3df678c61f42c6bda', 'balazon@gmail.com', 9123456789, 'Lipa City, Batangas');
 
 -- --------------------------------------------------------
 
@@ -167,11 +217,11 @@ INSERT INTO `tbempcontact` (`id`, `empid`, `password`, `email`, `contact_no`, `a
 DROP TABLE IF EXISTS `tbempinfo`;
 CREATE TABLE IF NOT EXISTS `tbempinfo` (
   `empid` int NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `firstname` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `department` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL,
   PRIMARY KEY (`empid`)
-) ENGINE=MyISAM AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbempinfo`
@@ -179,7 +229,83 @@ CREATE TABLE IF NOT EXISTS `tbempinfo` (
 
 INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
 (1, 'aguila', 'nina', 'cics'),
-(101, 'Balazon', 'Francis', 'CICS');
+(2, 'Reyes', 'Jones', 'Cics'),
+(3, 'Balazon', 'Francis', 'CICS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbeventdetail`
+--
+
+DROP TABLE IF EXISTS `tbeventdetail`;
+CREATE TABLE IF NOT EXISTS `tbeventdetail` (
+  `eventId` int NOT NULL AUTO_INCREMENT,
+  `eventWhere` char(50) DEFAULT NULL,
+  `eventWhen` char(50) DEFAULT NULL,
+  `eventWhat` char(50) DEFAULT NULL,
+  `eventWho` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`eventId`),
+  KEY `deptId_fk` (`eventWho`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbeventdetail`
+--
+
+INSERT INTO `tbeventdetail` (`eventId`, `eventWhere`, `eventWhen`, `eventWhat`, `eventWho`) VALUES
+(1, 'BSU LIPA CAMPUS', 'December 1,2023', 'CICS Week', 'All Students');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbeventview`
+--
+
+DROP TABLE IF EXISTS `tbeventview`;
+CREATE TABLE IF NOT EXISTS `tbeventview` (
+  `srCode` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `section` varchar(25) DEFAULT NULL,
+  `deptName` varchar(50) DEFAULT NULL,
+  `eventWhat` varchar(50) DEFAULT NULL,
+  `evdate` date DEFAULT NULL,
+  `evtime` time DEFAULT NULL,
+  KEY `section` (`section`),
+  KEY `eventWhat` (`eventWhat`),
+  KEY `deptName` (`deptName`),
+  KEY `srCode` (`srCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbeventview`
+--
+
+INSERT INTO `tbeventview` (`srCode`, `section`, `deptName`, `eventWhat`, `evdate`, `evtime`) VALUES
+('21-34890', 'SM-3101', 'CICS', 'CICS Week', '2023-11-22', '04:58:02'),
+('21-37506', 'SM-3101', 'CICS', 'CICS Week', '2023-11-22', '05:04:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsecurity`
+--
+
+DROP TABLE IF EXISTS `tbsecurity`;
+CREATE TABLE IF NOT EXISTS `tbsecurity` (
+  `adminId` int NOT NULL,
+  `adminUserN` varchar(25) DEFAULT NULL,
+  `adminPass` varchar(25) DEFAULT NULL,
+  `empid` int DEFAULT NULL,
+  PRIMARY KEY (`adminId`),
+  KEY `empid` (`empid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbsecurity`
+--
+
+INSERT INTO `tbsecurity` (`adminId`, `adminUserN`, `adminPass`, `empid`) VALUES
+(0, 'Admin1', 'Admin111', 2);
 
 -- --------------------------------------------------------
 
@@ -204,7 +330,34 @@ CREATE TABLE IF NOT EXISTS `tbstudcontact` (
 --
 
 INSERT INTO `tbstudcontact` (`id`, `studid`, `password`, `email`, `contact_no`, `address`) VALUES
-(1, 1001, '10c7ccc7a4f0aff03c915c485565b9da', 'ryan@gmail.com', 9123456789, 'Cuenca, Batangas');
+(1, 6, '5d41402abc4b2a76b9719d911017c592', 'ryan@gmail.com', 9123456789, 'Cuenca, Batangas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbstudent`
+--
+
+DROP TABLE IF EXISTS `tbstudent`;
+CREATE TABLE IF NOT EXISTS `tbstudent` (
+  `srCode` varchar(25) NOT NULL,
+  `studPass` varchar(50) DEFAULT NULL,
+  `deptName` varchar(50) DEFAULT NULL,
+  `section` varchar(25) NOT NULL,
+  `studid` int DEFAULT NULL,
+  PRIMARY KEY (`srCode`),
+  KEY `studid` (`studid`),
+  KEY `deptName` (`deptName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbstudent`
+--
+
+INSERT INTO `tbstudent` (`srCode`, `studPass`, `deptName`, `section`, `studid`) VALUES
+('21-34890', '123', 'CICS', 'SM-3101', 3),
+('21-36550', '123', 'CICS', 'SM-3101', 5),
+('21-37506', '123', 'CICS', 'SM-3101', 4);
 
 -- --------------------------------------------------------
 
@@ -215,11 +368,11 @@ INSERT INTO `tbstudcontact` (`id`, `studid`, `password`, `email`, `contact_no`, 
 DROP TABLE IF EXISTS `tbstudinfo`;
 CREATE TABLE IF NOT EXISTS `tbstudinfo` (
   `studid` int NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `firstname` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `course` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL,
   PRIMARY KEY (`studid`)
-) ENGINE=MyISAM AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tbstudinfo`
@@ -228,11 +381,34 @@ CREATE TABLE IF NOT EXISTS `tbstudinfo` (
 INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
 (1, 'parker', 'peter', 'bsit'),
 (2, 'kent', 'clark', 'bscs'),
-(1001, 'Ramos', 'Ryan Ceasar', 'BSIT'),
-(1002, 'Magnaye', 'Erika', 'BSIT'),
-(1003, 'Gabuya', 'Jenny Mae', 'BSIT'),
-(1004, 'Peloramas', 'Jelladane', 'BSIT'),
-(1005, 'Dimaculangan', 'Everson', 'BSIT');
+(3, 'Roxas', 'Kent', 'bsit'),
+(4, 'Custodio', 'Juliana Beatriz', 'bsit'),
+(5, 'Lasay', 'John Lloyd', 'bsit'),
+(6, 'Ramos', 'Ryan Ceasar', 'BSIT');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbeventview`
+--
+ALTER TABLE `tbeventview`
+  ADD CONSTRAINT `tbeventview_ibfk_2` FOREIGN KEY (`deptName`) REFERENCES `tbdepartment` (`deptName`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbeventview_ibfk_3` FOREIGN KEY (`srCode`) REFERENCES `tbstudent` (`srCode`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbsecurity`
+--
+ALTER TABLE `tbsecurity`
+  ADD CONSTRAINT `tbsecurity_ibfk_1` FOREIGN KEY (`empid`) REFERENCES `tbempinfo` (`empid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbstudent`
+--
+ALTER TABLE `tbstudent`
+  ADD CONSTRAINT `tbstudent_ibfk_1` FOREIGN KEY (`studid`) REFERENCES `tbstudinfo` (`studid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbstudent_ibfk_2` FOREIGN KEY (`deptName`) REFERENCES `tbdepartment` (`deptName`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
