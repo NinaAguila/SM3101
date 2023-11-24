326f6ddb284c56a869cef7eddf499a9280077910
Janine Lansangan <142392401+JanineLansangan@users.noreply.github.com>
2023-11-24 14:31:12 +0800

    db_sm3101.sql
    Group 5 and Group 4

--- db_sm3101.sql
+++ db_sm3101.sql
@@ -189,3 +189,268 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 24, 2023 at 05:57 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_sm3101`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbempinfo`
+--
+
+DROP TABLE IF EXISTS `tbempinfo`;
+CREATE TABLE IF NOT EXISTS `tbempinfo` (
+  `empid` int NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  PRIMARY KEY (`empid`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbempinfo`
+--
+
+INSERT INTO `tbempinfo` (`empid`, `lastname`, `firstname`, `department`) VALUES
+(1, 'aguila', 'nina', 'cics'),
+(2, 'libunao', 'angeline', 'cics'),
+(3, 'suarez', 'aileen', 'cics'),
+(4, 'sulit', 'richelle', 'cics'),
+(5, 'reyes', 'christoper', 'cics'),
+(10, 'austria', 'carl jerome', 'cics'),
+(9, 'balazon', 'francis', 'cics'),
+(8, 'cabrera', 'christian', 'cics'),
+(7, 'labiaga', 'elenor', 'cics'),
+(6, 'alimoren', 'dioneces', 'cics'),
+(11, 'admin', 'one', 'security'),
+(12, 'admin', 'two', 'security'),
+(13, 'guard', 'one', 'security'),
+(14, 'guard', 'two', 'security'),
+(15, 'admin ', 'three', 'security');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbitems`
+--
+
+DROP TABLE IF EXISTS `tbitems`;
+CREATE TABLE IF NOT EXISTS `tbitems` (
+  `ItemID` int NOT NULL AUTO_INCREMENT,
+  `ItemName` varchar(100) NOT NULL,
+  `ItemDescription` text,
+  `DateTime` datetime DEFAULT NULL,
+  `Location` varchar(100) DEFAULT NULL,
+  `Status` enum('Lost','Found','Claimed') NOT NULL,
+  `Category` varchar(50) DEFAULT NULL,
+  `ReportedBy` int DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ItemID`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbitems`
+--
+
+INSERT INTO `tbitems` (`ItemID`, `ItemName`, `ItemDescription`, `DateTime`, `Location`, `Status`, `Category`, `ReportedBy`, `Image`) VALUES
+(1, 'Lost Phone', 'Black smartphone with cracked screen.', '2023-10-01 10:30:00', 'Library', 'Lost', 'Electronics', 3, 'phone.jpg'),
+(2, 'Lost Backpack', 'Red backpack with textbooks and a laptop.', '2023-10-02 15:45:00', 'Cafeteria', 'Lost', 'Bags', 4, 'backpack.jpg'),
+(3, 'Lost Keys', 'Set of house and car keys.', '2023-10-03 08:15:00', 'Parking Lot', 'Found', 'Keys', 5, 'keys.jpg'),
+(4, 'Lost Wallet', 'Brown leather wallet with cash and ID.', '2023-10-04 17:20:00', 'Gym', 'Lost', 'Wallets', 6, 'wallet.jpg'),
+(5, 'Umbrella', 'Blue umbrella with a pattern.', '2023-10-05 12:00:00', 'Bus Stop', 'Claimed', 'Accessories', 7, 'umbrella.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbrecentactivities`
+--
+
+DROP TABLE IF EXISTS `tbrecentactivities`;
+CREATE TABLE IF NOT EXISTS `tbrecentactivities` (
+  `ActivityID` int NOT NULL AUTO_INCREMENT,
+  `ActivityType` varchar(20) NOT NULL,
+  `UserID` int DEFAULT NULL,
+  `ItemID` int DEFAULT NULL,
+  `DateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`ActivityID`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbrecentactivities`
+--
+
+INSERT INTO `tbrecentactivities` (`ActivityID`, `ActivityType`, `UserID`, `ItemID`, `DateTime`) VALUES
+(1, 'Report', 3, 1, '2023-10-01 10:30:00'),
+(2, 'Report', 4, 2, '2023-10-02 15:45:00'),
+(3, 'Report', 5, 3, '2023-10-03 08:15:00'),
+(4, 'Report', 6, 4, '2023-10-04 17:20:00'),
+(5, 'Report', 7, 5, '2023-10-05 12:00:00'),
+(6, 'Report', 8, 6, '2023-10-06 09:30:00'),
+(7, 'Report', 9, 7, '2023-10-07 14:10:00'),
+(8, 'Report', 10, 8, '2023-10-08 11:05:00'),
+(9, 'Report', 3, 9, '2023-10-09 16:30:00'),
+(10, 'Report', 4, 10, '2023-10-10 13:25:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbreports`
+--
+
+DROP TABLE IF EXISTS `tbreports`;
+CREATE TABLE IF NOT EXISTS `tbreports` (
+  `ReportID` int NOT NULL AUTO_INCREMENT,
+  `ReportName` varchar(255) DEFAULT NULL,
+  `ReportDescription` text,
+  `DateTime` datetime DEFAULT NULL,
+  `Location` varchar(255) DEFAULT NULL,
+  `Category` varchar(255) DEFAULT NULL,
+  `ReportedBy` varchar(255) DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ReportID`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbreports`
+--
+
+INSERT INTO `tbreports` (`ReportID`, `ReportName`, `ReportDescription`, `DateTime`, `Location`, `Category`, `ReportedBy`, `Image`) VALUES
+(1, 'Lost Watch', 'Gold wristwatch with a leather strap', '2023-11-01 15:45:00', 'Library', 'Accessories', 'Eva Johnson', 'watch.jpg'),
+(2, 'Found Umbrella', 'Red umbrella left near the entrance', '2023-11-05 11:30:00', 'CECS Building', 'Accessories', 'Mark Anderson', 'umbrella.jpg'),
+(3, 'Missing Glasses', 'Black rectangular eyeglasses', '2023-11-10 14:00:00', 'HEB Building', 'Accessories', 'Sophia Lee', 'glasses.jpg'),
+(4, 'Lost Laptop Charger', 'Dell laptop charger with a black cable', '2023-11-12 16:15:00', 'Facade', 'Electronics', 'Daniel Williams', 'charger.jpg'),
+(5, 'Found Wallet', 'Brown leather wallet with credit cards', '2023-11-15 13:50:00', 'Comfort Room', 'Personal Items', 'Olivia Taylor', 'wallet.jpg'),
+(6, 'Missing Backpack', 'Gray backpack with a laptop inside', '2023-11-18 09:30:00', 'Library', 'Bags', 'Benjamin Davis', 'backpack.jpg'),
+(7, 'Lost ID Badge', 'Company ID badge with a blue lanyard', '2023-11-20 10:00:00', 'CECS Building', 'Accessories', 'Emma Wilson', 'badge.jpg'),
+(8, 'Found Bicycle', 'Blue mountain bike near the bike rack', '2023-11-22 14:45:00', 'HEB Building', 'Sports Equipment', 'Liam Martinez', 'bike.jpg'),
+(9, 'Missing Wallet', 'Green wallet with driver\'s license', '2023-11-25 12:30:00', 'Facade', 'Personal Items', 'Aiden Brown', 'green_wallet.jpg'),
+(10, 'Found Headphones', 'Wireless headphones in a black case', '2023-11-28 15:20:00', 'Comfort Room', 'Electronics', 'Mia Davis', 'headphones.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsearchhistory`
+--
+
+DROP TABLE IF EXISTS `tbsearchhistory`;
+CREATE TABLE IF NOT EXISTS `tbsearchhistory` (
+  `SearchID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
+  `SearchQuery` text NOT NULL,
+  `SearchDateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`SearchID`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbsearchhistory`
+--
+
+INSERT INTO `tbsearchhistory` (`SearchID`, `UserID`, `SearchQuery`, `SearchDateTime`) VALUES
+(1, 3, 'Lost phone', '2023-10-02 10:30:00'),
+(2, 3, 'Found laptop', '2023-10-05 14:15:00'),
+(3, 4, 'Lost keys', '2023-10-07 08:45:00'),
+(4, 4, 'Lost glasses', '2023-10-10 11:30:00'),
+(5, 5, 'Found wallet', '2023-10-12 18:00:00'),
+(6, 5, 'Found backpack', '2023-10-14 14:30:00'),
+(7, 6, 'Lost umbrella', '2023-10-15 09:45:00'),
+(8, 6, 'Lost ID card', '2023-10-18 16:15:00'),
+(9, 7, 'Found watch', '2023-10-19 14:10:00'),
+(10, 7, 'Lost laptop', '2023-10-20 10:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbstudinfo`
+--
+
+DROP TABLE IF EXISTS `tbstudinfo`;
+CREATE TABLE IF NOT EXISTS `tbstudinfo` (
+  `studid` int NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(25) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `course` varchar(20) NOT NULL,
+  PRIMARY KEY (`studid`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbstudinfo`
+--
+
+INSERT INTO `tbstudinfo` (`studid`, `lastname`, `firstname`, `course`) VALUES
+(1, 'parker', 'peter', 'bsit'),
+(2, 'kent', 'clark', 'bsit'),
+(3, 'coronel', 'marielle', 'bsit'),
+(4, 'san pablo', 'yukari', 'bsit'),
+(5, 'custodio', 'bea', 'bsit'),
+(6, 'saltoc', 'cheniell', 'bsit'),
+(7, 'rubico', 'paulo', 'bsit'),
+(8, 'lansangan', 'janine', 'bsit'),
+(9, 'borja', 'john laurence', 'bsit'),
+(10, 'fajilan', 'curlyne', 'bsit');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbusers`
+--
+
+DROP TABLE IF EXISTS `tbusers`;
+CREATE TABLE IF NOT EXISTS `tbusers` (
+  `userid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `usertype` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `empid` int DEFAULT NULL,
+  `studid` int DEFAULT NULL,
+  PRIMARY KEY (`userid`),
+  KEY `empid` (`empid`),
+  KEY `studid` (`studid`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbusers`
+--
+
+INSERT INTO `tbusers` (`userid`, `username`, `password`, `usertype`, `status`, `email`, `empid`, `studid`) VALUES
+(1, 'nina_aguila', 'empaguila111', 'client', 'active', 'ninaaguila@gmail.com', 1, NULL),
+(2, 'angeline_libunao', 'emplibunao222', 'client', 'active', 'libunaoange@gmail.com', 2, NULL),
+(3, 'aileen_suarez', 'empsuarez333', 'client', 'active', 'aileensuarez@gmail.com', 3, NULL),
+(4, 'richelle_sulit', 'empsulit444', 'client', 'active', 'richellesulit@gmail.com', 4, NULL),
+(5, 'chris_reyes', 'empreyes555', 'client', 'active', 'chrisreyes@gmail.com', 5, NULL),
+(6, 'peter_parker', 'userpass6', 'client', 'active', 'ninaaguila@gmail.com', NULL, 1),
+(7, 'kent_clark', 'userpass7', 'client', 'active', 'ninaaguila@gmail.com', NULL, 2),
+(8, 'coronel_marielle', 'userpass8', 'client', 'active', 'coronelmarielle@gmail.com', NULL, 3),
+(9, 'yukari_sanpablo', 'userpass9', 'client', 'active', 'yukarisanpablo@gmail.com', NULL, 4),
+(10, 'bea_custodio', 'userpass10', 'client', 'active', 'custodiobea@gmail.com', NULL, 5),
+(11, 'admin_user1', 'adminpass1', 'admin', 'active', 'adminuser1@gmail.com', 11, NULL),
+(12, 'admin_user2', 'adminpass2', 'admin', 'active', 'adminuser2@gmail.com', 12, NULL),
+(13, 'admin_user3', 'adminpass3', 'admin', 'active', 'adminuser3@gmail.com', 13, NULL),
+(14, 'guard_user1', 'guardpass1', 'guard', 'active', 'guarduser1@gmail.com', 14, NULL),
+(15, 'guard_user2', 'guardpass2', 'guard', 'active', 'guarduser2@gmail.com', 15, 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
