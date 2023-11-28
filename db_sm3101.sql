ea67ed3323196a09c73b22eda2af03d7bd1be30f
Savior-Tipan <119476814+Savior-Tipan@users.noreply.github.com>
2023-11-28 21:40:16 +0800

    Update db_sm3101.sql

--- db_sm3101.sql
+++ db_sm3101.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2023 at 01:33 AM
+-- Generation Time: Nov 28, 2023 at 02:37 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -243,6 +243,7 @@ ALTER TABLE `tbstudinfo`
 ALTER TABLE `tbusers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
